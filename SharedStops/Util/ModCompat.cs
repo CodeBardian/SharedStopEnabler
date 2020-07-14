f90c75cd15b700469aaad7db786018b58b1ab0e8
CodeBardian <jacksw247@gmail.com>
2020-07-14 16:35:52 +0200

    mod compatibility

---
+++ SharedStops/Util/ModCompat.cs
@@ -0,0 +1,28 @@
+﻿using ColossalFramework;
+using ColossalFramework.Plugins;
+using ICities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+using static ColossalFramework.Plugins.PluginManager;
+
+namespace SharedStopEnabler.Util
+{
+    static class ModCompat
+    {
+        public static Dictionary<ulong, PluginInfo> foundMods = new Dictionary<ulong, PluginInfo>();
+
+        public static void ScanMods()
+        {
+            foreach (PluginInfo mod in Singleton<PluginManager>.instance.GetPluginsInfo())
+            {
+                if (mod.isBuiltin || mod.isCameraScript) continue;
+
+                Log.Info($"found mod {mod.publishedFileID.AsUInt64} {mod.name} {((IUserMod)mod.userModInstance).Name}");
+                foundMods.Add(mod.publishedFileID.AsUInt64, mod);                  
+            }
+        }
+    }
+}

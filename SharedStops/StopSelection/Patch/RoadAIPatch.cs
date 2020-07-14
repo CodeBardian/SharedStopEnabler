893c5ec5ab85517e550117562858b2e9b7ec32e3
CodeBardian <jacksw247@gmail.com>
2020-07-14 18:47:33 +0200

    catch nullref exception

--- SharedStops/StopSelection/Patch/RoadAIPatch.cs
+++ SharedStops/StopSelection/Patch/RoadAIPatch.cs
@@ -17,19 +17,15 @@ namespace SharedStopEnabler.StopSelection.Patch
 		[RedirectMethod]
 		public override void UpdateSegmentFlags(ushort segmentID, ref NetSegment data)
 		{
-			// calls base.UpdateSegmentFlags(segmentID, ref data);
-			//object[] args = new object[] { segmentID, data };
-			//object result = typeof(RoadBaseAI).GetMethod("UpdateSegmentFlags", new[] { typeof(ushort), typeof(NetSegment).MakeByRefType() }).Invoke(this, args);
-			//if ((bool)result)
-			//	data = (NetSegment)args[1];
+			// rather hacky call to base.UpdateSegmentFlags(segmentID, ref data);
 			try
 			{
-				MethodInfo method = typeof(RoadBaseAI).GetMethod("UpdateSegmentFlags", new[] { typeof(ushort), typeof(NetSegment).MakeByRefType()});
-				Log.Debug($"{method}");
+				MethodInfo method = typeof(RoadBaseAI).GetMethod("UpdateSegmentFlags", BindingFlags.Instance | BindingFlags.Public, null, new[] { typeof(ushort), typeof(NetSegment).MakeByRefType()}, null);
+				Log.Info($"{method}");
 				IntPtr ptr = method.MethodHandle.GetFunctionPointer();
-				Log.Debug($"{ptr}");
+				Log.Info($"{ptr}");
 				Action<ushort, NetSegment> baseUpdate = (Action<ushort, NetSegment>)Activator.CreateInstance(typeof(Action<ushort, NetSegment>), this, ptr);
-				Log.Debug($"{baseUpdate}");
+				Log.Info($"{baseUpdate}");
 				baseUpdate(segmentID, data);
 			}
 			catch (Exception e)

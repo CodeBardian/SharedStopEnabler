d8708324fe1b28d6d92c7e1ad54575b7f506cb21
CodeBardian <jacksw247@gmail.com>
2023-03-23 20:59:56 +0100

    change patch siganture to reflect changes from hubs and transport update

--- SharedStops/StopSelection/Patch/NetSegmentPatch.cs
+++ SharedStops/StopSelection/Patch/NetSegmentPatch.cs
@@ -8,13 +8,13 @@ namespace SharedStopEnabler.StopSelection.Patch
     [HarmonyPatch(typeof(NetSegment))]
     [HarmonyPatch("GetClosestLanePosition")]
     [HarmonyPatch(
-        new Type[] { typeof(Vector3), typeof(NetInfo.LaneType), typeof(VehicleInfo.VehicleType), typeof(VehicleInfo.VehicleType),
+        new Type[] { typeof(Vector3), typeof(NetInfo.LaneType), typeof(VehicleInfo.VehicleType), typeof(VehicleInfo.VehicleCategory), typeof(VehicleInfo.VehicleType),
             typeof(bool), typeof(Vector3), typeof(int), typeof(float), typeof(Vector3), typeof(int), typeof(float)},
-        new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal,
+        new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal,
             ArgumentType.Out, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out})]
     class NetSegmentPatch_GetClosestLanePosition
     {
-        static bool Prefix(ref bool __result, NetSegment __instance, Vector3 point, NetInfo.LaneType laneTypes, VehicleInfo.VehicleType vehicleTypes, ref bool requireConnect)
+        static bool Prefix(ref bool __result, NetSegment __instance, ref bool requireConnect)
         {
             if (requireConnect && __instance.Info.m_netAI is RoadBridgeAI)
             {
--- SharedStops/StopSelection/Patch/PathManagerPatch.cs
+++ SharedStops/StopSelection/Patch/PathManagerPatch.cs
@@ -13,20 +13,20 @@ namespace SharedStopEnabler.StopSelection.Patch
     [HarmonyPatch(typeof(PathManager))]
     [HarmonyPatch("FindPathPosition")]
     [HarmonyPatch(
-        new Type[] { typeof(Vector3), typeof(ItemClass.Service), typeof(ItemClass.Service), typeof(NetInfo.LaneType), typeof(VehicleInfo.VehicleType), 
-            typeof(VehicleInfo.VehicleType), typeof(bool), typeof(bool), typeof(float), typeof(PathUnit.Position), typeof(PathUnit.Position), typeof(float), typeof(float)},
-        new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal,
-            ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out})]
+        new Type[] { typeof(Vector3), typeof(ItemClass.Service), typeof(ItemClass.Service), typeof(NetInfo.LaneType), typeof(VehicleInfo.VehicleType), typeof(VehicleInfo.VehicleCategory),
+            typeof(VehicleInfo.VehicleType), typeof(bool), typeof(bool), typeof(float), typeof(bool), typeof(PathUnit.Position), typeof(PathUnit.Position), typeof(float), typeof(float)},
+        new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal,
+            ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out})]
     class PathManagerPatch_FindPathPosition
     {
-        static void Postfix(Vector3 position, ref PathUnit.Position pathPosA, VehicleInfo.VehicleType stopType)
+        static void Postfix(Vector3 position, ref PathUnit.Position pathPosA, VehicleInfo.VehicleType stopType, VehicleInfo.VehicleCategory vehicleCategory)
         {
             NetSegment segment = Singleton<NetManager>.instance.m_segments.m_buffer[pathPosA.m_segment];
             int stop = pathPosA.m_lane;
 
             if (stopType == VehicleInfo.VehicleType.Car && stop > 1)
             {  
-                segment.GetClosestLanePosition(position, NetInfo.LaneType.Vehicle, stopType, out Vector3 pos, out uint laneID, out int laneindex, out float laneOffset);
+                segment.GetClosestLanePosition(position, NetInfo.LaneType.Vehicle, stopType, vehicleCategory, out Vector3 pos, out uint laneID, out int laneindex, out float laneOffset);
 
                 NetInfo.Direction dir = segment.Info.m_lanes[laneindex].m_direction;
 
